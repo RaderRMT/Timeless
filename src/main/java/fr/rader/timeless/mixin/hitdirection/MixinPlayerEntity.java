88983b953519c7633a8f664dacfef3e1889130ef
RaderRMT <raderpy@protonmail.com>
2023-04-14 21:19:18 +0200

    better hit direction mixin

--- src/main/java/fr/rader/timeless/mixin/hitdirection/MixinPlayerEntity.java
+++ src/main/java/fr/rader/timeless/mixin/hitdirection/MixinPlayerEntity.java
@@ -7,6 +7,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity {
@@ -18,13 +20,17 @@ public abstract class MixinPlayerEntity extends LivingEntity {
         super(entityType, world);
     }
 
-    @Override
-    public void animateDamage(float yaw) {
+
+    @ModifyVariable(
+            method = "animateDamage",
+            at = @At("HEAD"),
+            argsOnly = true
+    )
+    public float disableHitDirection(float yaw) {
         if (Timeless.getConfig().disableHitDirection) {
-            yaw = 0;
+            return 0;
         }
 
-        super.animateDamage(yaw);
-        this.damageTiltYaw = yaw;
+        return yaw;
     }
 }

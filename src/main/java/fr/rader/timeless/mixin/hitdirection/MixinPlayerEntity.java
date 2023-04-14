b191bd052123f2c393ffd0c8748c626ca6f53968
RaderRMT <raderpy@protonmail.com>
2023-04-14 21:29:27 +0200

    removed empty space

--- src/main/java/fr/rader/timeless/mixin/hitdirection/MixinPlayerEntity.java
+++ src/main/java/fr/rader/timeless/mixin/hitdirection/MixinPlayerEntity.java
@@ -6,20 +6,15 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity {
 
-    @Shadow
-    protected float damageTiltYaw;
-
     protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
-
 
     @ModifyVariable(
             method = "animateDamage",

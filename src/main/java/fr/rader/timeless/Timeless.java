2df91c298b8221db5f75deb97a77bd1102f73396
RaderRMT <raderpy@protonmail.com>
2023-06-11 00:51:48 +0200

    keep only ClientModInitializer

--- src/main/java/fr/rader/timeless/Timeless.java
+++ src/main/java/fr/rader/timeless/Timeless.java
@@ -2,14 +2,14 @@ package fr.rader.timeless;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
-public class Timeless implements ModInitializer {
+public class Timeless implements ClientModInitializer {
 
     private static TimelessConfig config;
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         AutoConfig.register(TimelessConfig.class, GsonConfigSerializer::new);
 
         Timeless.config = AutoConfig.getConfigHolder(TimelessConfig.class).getConfig();
